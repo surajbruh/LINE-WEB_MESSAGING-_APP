fb86f07e9a4abb57bb6e6d1445f9bc332220ee45
suraj <imsurajyadav044@gmail.com>
2025-09-25 14:53:15 +0530

    fixed socket.js BASE_URL var

--- FRONTEND/src/socket.js
+++ FRONTEND/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:4000"
+const BASE_URL = import.meta.env.VITE_API_URL
 export const socket = io(BASE_URL, {
     withCredentials: true,
     autoConnect: false,
