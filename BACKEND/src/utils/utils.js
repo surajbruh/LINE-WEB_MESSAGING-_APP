b711c75e1b9b5a23204b6995cfcdcbe2f3b19503
suraj <imsurajyadav044@gmail.com>
2025-09-25 21:17:14 +0530

    updated cookieOptions from utils.js

--- BACKEND/src/utils/utils.js
+++ BACKEND/src/utils/utils.js
@@ -2,6 +2,6 @@ export const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    sameSite: (process.env.NODE_ENV === "production") ? "none" : "strict",
     maxAge: 60 * 60 * 1000
 }
